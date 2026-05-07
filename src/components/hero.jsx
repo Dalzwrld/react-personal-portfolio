8efb9c6e143848bac8b4efeb6b401be42cf667bd
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-05-07 23:46:03 +0300

    feat: add hero jsx file

---
+++ src/components/hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className="">
+        <nav className="">
+            <h3 className=""></h3>
+            <ul className="">
+                <li className=""></li>
+                <li className=""></li>
+                <li className=""></li>
+            </ul>
+        </nav>
+
+        <div className=""> 
+            <h1 className=""></h1>
+            <h3 className=""></h3>
+            {/* <button className=""></button> */}
+        </div>
+    </div>
+  )
+}
+
+export default Hero
